4c62d7042afc7198ee7eaf490d2ffb86abe188f8
Kekelberi <alexei.gaponov@gmail.com>
2022-02-21 18:06:18 +0300

    save

--- SocialLinks/SocialLinks.js
+++ SocialLinks/SocialLinks.js
@@ -39,7 +39,7 @@ const actions = [
   {
     name: "Instagram",
     title: "Instagram",
-    href: "https://www.facebook.com/nysesh",
+    href: "",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z" />
